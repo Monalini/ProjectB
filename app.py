7c735546ece68076f3b0a33e4548ba79e200084c
Monalini <monaligaikwad14@gmail.com>
2022-08-30 18:41:44 +0530

    flask api added

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 #This is python file
+
+import pandas as pd
+import numpy as np 
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'default API'
+    
