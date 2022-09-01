02046495d154fca251503af0fb103e7b05705e0e
Monalini <monaligaikwad14@gmail.com>
2022-09-01 05:56:56 +0530

    added api for prediction

--- app.py
+++ app.py
@@ -10,4 +10,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'default API'
-    
+
+
+@app.route("/path")
+def path_logic():
+    return " Path API"
+if __name__ == "__main__":
+    app.run(debug = True)
